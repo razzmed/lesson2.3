158812a180c1c7299645110e0de56b75eedfabe2
razzmed <mederrazzakov@gmail.com>
2020-01-28 23:52:29 +0600

    Hello

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        public static void printText() {
+            System.out.println("Hello");
+            System.out.println("Java");
+            System.out.println("Geektech");
+        }
+    }
+}
